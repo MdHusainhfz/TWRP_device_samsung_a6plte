0b84a20a20e71446316b3ba47e30ea4c52d37f6d
Md Husain Shikder <56667038+MdHusainhfz@users.noreply.github.com>
2022-12-06 17:58:49 +0600

    Fixes mistake

--- device.mk
+++ device.mk
@@ -6,7 +6,7 @@
 #
 
 # Enable project quotas and casefolding for emulated storage without sdcardfs
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # VNDK
 PRODUCT_TARGET_VNDK_VERSION := 30
